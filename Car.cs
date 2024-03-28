b285dd7535a1414575810ca8724e00cc5d9c3b74
Emma Forsberg <forsberg.emma@outlook.com>
2024-03-28 14:18:23 +0100

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicle
+{
+    internal class Car : IVehicle
+    {
+        public int Speed { get; set; }
+
+        public int GetSpeed()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int SetSpeed()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ IVehicle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicle
+{
+    internal interface IVehicle
+    {
+        int Speed { get; set; }
+
+        int SetSpeed();
+        int GetSpeed();
+
+    }
+}
