354eaccb9b2dbfd009a60ff01f0fbc3f2a8d0d3c
Frida Gidlund <frida_gidde95@hotmail.com>
2024-04-07 15:46:30 +0200

    revision 2

--- IVehicle.cs
+++ IVehicle.cs
@@ -2,16 +2,19 @@
 {
     internal enum VehicleEnum
     {
-        Car = 1,
+		// https://www.codingame.com/playgrounds/2487/c---how-to-display-friendly-names-for-enumerations
+		Car = 1,
         Boat = 2,
         Motorcycle = 3
     }
     internal interface IVehicle
     {
-        int speed { get;}
-        string SpeedMeasurement { get;}
+		                     // publika variabler bör vara med CamelCase
+        //int speed { get; } // tror det var jag som introducerade denna men eftersom GetSpeed och SetSpeed finns så bör den kanske inte finnas (vad är syftet att kunna komma åt den via interfacet)
+                             // detta hade kanske varit mer lämligt om detta varit en basklass
+		string SpeedMeasurement { get;}
 
-        void SetSpeed(int speed);
+        void SetSpeed(int speed); // vill bara vara tydlig med att denna "speed" inte är samma som ovan variabel
         int GetSpeed();
 
     }
